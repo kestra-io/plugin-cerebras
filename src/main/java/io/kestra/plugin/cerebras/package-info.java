0eb5f0dab27c2822146002cc39b379fd080d1b71
François Delbrayelle <fdelbrayelle@kestra.io>
2026-05-29 15:23:06 +0200

    chore: initial scaffold cleanup for plugin-cerebras

---
+++ src/main/java/io/kestra/plugin/cerebras/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Cerebras",
+    description = "Cerebras plugin for Kestra",
+    categories = PluginSubGroup.PluginCategory.DATA
+)
+package io.kestra.plugin.cerebras;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
